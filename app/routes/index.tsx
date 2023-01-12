0efeada74046ad1a8a42826d08441ec518c28d3d
jaredkc <jared@clivecoffee.com>
2023-01-11 18:04:39 -0700

    add tailwindcss

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,14 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
+    <div className="p-4 md:p-8">
+      <h1 className="text-3xl font-light mb-4">Welcome to Remix</h1>
+      <ul className="list-disc pl-4">
         <li>
           <a
             target="_blank"
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
+            className="underline text-blue-800"
           >
             15m Quickstart Blog Tutorial
           </a>
@@ -17,12 +18,18 @@ export default function Index() {
             target="_blank"
             href="https://remix.run/tutorials/jokes"
             rel="noreferrer"
+            className="underline text-blue-800"
           >
             Deep Dive Jokes App Tutorial
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer"
+            className="underline text-blue-800"
+          >
             Remix Docs
           </a>
         </li>
