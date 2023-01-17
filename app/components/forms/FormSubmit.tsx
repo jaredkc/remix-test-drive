5316ec66c80f62cf495b7541ef62a16fc20a91c8
jaredkc <jared@clivecoffee.com>
2023-01-17 11:14:15 -0700

    button spacing

--- app/components/forms/FormSubmit.tsx
+++ app/components/forms/FormSubmit.tsx
@@ -6,7 +6,7 @@ export const FormSubmit = () => {
     <button
       type="submit"
       disabled={isSubmitting}
-      className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      className="flex justify-center w-full px-4 py-2 mt-6 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
     >
       {isSubmitting ? 'Submitting...' : 'Submit'}
     </button>
