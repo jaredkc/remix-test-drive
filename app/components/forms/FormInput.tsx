8e5bf63e68ce7a04279638d00e218078c894bcad
jaredkc <jared@clivecoffee.com>
2023-01-17 11:14:03 -0700

    error styles

--- app/components/forms/FormInput.tsx
+++ app/components/forms/FormInput.tsx
@@ -11,15 +11,18 @@ export const FormInput = ({ name, label, type }: FormInputProps) => {
 
   return (
     <div className="flex flex-col gap-1 my-4">
-      <label htmlFor={name} className="block text-sm font-medium text-gray-700">
-        {label}
+      <label htmlFor={name} className="block text-xs font-medium text-gray-700">
+        {error ? (
+          <span className="pl-1 text-xs text-red-800">{error}</span>
+        ) : (
+          label
+        )}
       </label>
       <input
         {...getInputProps({ id: name })}
         type={type}
         className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
       />
-      {error && <span className="my-error-class">{error}</span>}
     </div>
   );
 };
