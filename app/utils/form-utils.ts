c19438d24082cb1862d5150f8cdc31bf2c08c81d
jaredkc <jared@clivecoffee.com>
2023-03-11 08:15:23 -0700

    remove what is not being used

--- app/utils/form-utils.ts
+++ app/utils/form-utils.ts
@@ -1,15 +1,3 @@
-export function isSecurePassword(password: string): boolean {
-  // Password must be at least 8 characters long, contain at least one uppercase letter,
-  // one lowercase letter, one number, and can have special characters.
-  // (?=.*[a-z]) - at least one lowercase letter
-  // (?=.*[A-Z]) - at least one uppercase letter
-  // (?=.*\d) - at least one number
-  // [a-zA-Z0-9_!@#$%^&*()+?><.=\d]{8,} - at least 8 characters, special characters are optional
-  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9_!@#$%^&*()+?><.=\d]{8,}$/;
-  const passwordTest = re.test(password);
-  return passwordTest;
-}
-
 function randomCharacters(characters: string, count: number): string[] {
   const items = [];
   const array = characters.toString().split('');
@@ -39,32 +27,3 @@ export function generateIdAttr(value: string): string {
   return value.replace(/\W+/g, '-').toLowerCase();
 }
 
-/**
- * Find all form data keys that start with a prefix ('variety--',
- * and return an array without the prefix.
- */
-export function manyFormDataToArray(formData: FormData, find: string) {
-  const result = [];
-  const findName = find + '--';
-  for (let key of formData.keys()) {
-    if (key.startsWith(findName)) {
-      result.push(key.replace(findName, ''));
-    }
-  }
-  return result;
-}
-
-/**
- * Find all form data keys that start with a prefix ('country--'),
- * and return comma separated string without the prefix.
- */
-export function manyFormDataToString(formData: FormData, find: string) {
-  const result = [];
-  const findName = find + '--';
-  for (let key of formData.keys()) {
-    if (key.startsWith(findName)) {
-      result.push(key.replace(findName, ''));
-    }
-  }
-  return result.join(', ');
-}
