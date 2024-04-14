59fafbf52b6d7b67d7e43996b6fc477acc4e9acb
jaredkc <jaredkc@gmail.com>
2024-04-13 19:18:29 -0600

    Refine Loading css transition

--- app/components/AppFrame.tsx
+++ app/components/AppFrame.tsx
@@ -92,21 +92,14 @@ function AppNavigation(props: HTMLAttributes<HTMLElement>) {
 
 function Loading() {
   const [width, setWidth] = useState(0);
-  const [duration, setDuration] = useState('3000ms');
 
   useEffect(() => {
-    setWidth(0.5);
-    const timer1 = setTimeout(() => {
-      setWidth(0.6);
-      setDuration('1000ms');
-    }, 3000);
-    const timer2 = setTimeout(() => {
-      setWidth(0.7);
-      setDuration('2000ms');
-    }, 4000);
-    const timer3 = setTimeout(() => setWidth(0.8), 6000);
-    const timer4 = setTimeout(() => setWidth(0.9), 8000);
-    const timer5 = setTimeout(() => setWidth(0.99), 10000);
+    setWidth(0.2);
+    const timer1 = setTimeout(() => setWidth(0.4), 500);
+    const timer2 = setTimeout(() => setWidth(0.6), 1000);
+    const timer3 = setTimeout(() => setWidth(0.8), 3000);
+    const timer4 = setTimeout(() => setWidth(0.9), 6000);
+    const timer5 = setTimeout(() => setWidth(0.99), 9000);
 
     return () => {
       clearTimeout(timer1);
@@ -119,8 +112,8 @@ function Loading() {
 
   return (
     <div
-      className="fixed top-0 left-0 w-full h-0.5 origin-left linear bg-gradient-to-r from-cyan-500 to-blue-500"
-      style={{ transform: `scaleX(${width})`, animationDuration: duration }}
+      className="fixed top-0 left-0 w-full h-0.5 origin-left transition duration-300 bg-gradient-to-r from-cyan-400 to-indigo-400"
+      style={{ transform: `scaleX(${width})` }}
     >
       <span className="sr-only">Loading...</span>
     </div>
